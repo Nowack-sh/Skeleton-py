96163badfafd2da9f5c639e8c497b82e28e8e4c6
Nowack.sh <73441963+Nowack-sh@users.noreply.github.com>
2021-09-17 15:55:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import discord
+import discord #Importing the discord.py module
 
-bot = commands.Bot(command_prefix="Your prefix")
+bot = commands.Bot(command_prefix="Your prefix") #Creating a bot variable and configure a prefix
 
-@bot.command()
-async def test(ctx):
-  await ctx.send("The bot is working !")
+@bot.command() #Creating a command
+async def test(ctx): #Set the name of the command
+  await ctx.send("The bot is working !") #Set instructions when the command is used ("here sends "The bot is working")
 
-bot.run("Your token")
+bot.run("Your token") #Run the bot with the token
